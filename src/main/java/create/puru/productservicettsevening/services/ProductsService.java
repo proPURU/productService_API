e1edcc8eb4ff4e2a37083763b5161911b545362b
newPURU <purushattom.personal@gmail.com>
2024-06-01 01:50:21 +0530

    pagination implementation added

--- src/main/java/create/puru/productservicettsevening/services/ProductsService.java
+++ src/main/java/create/puru/productservicettsevening/services/ProductsService.java
@@ -2,18 +2,19 @@ package create.puru.productservicettsevening.services;
 
 import create.puru.productservicettsevening.dtos.ProductDTO;
 import create.puru.productservicettsevening.models.Product;
-import org.springframework.http.HttpStatusCode;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Spliterator;
 
 public interface ProductsService {
 
     List<Product> GetAllProducts();
+
+    public List<Product> GetAllProductsByPagination();
+
     Product addNewProduct(
             ProductDTO product
-            );
+    );
     Optional<Product> getSingleProduct(Long productId);
     Product updateProducts( Long productId,Product product);
     String deleteProducts(Long productId);
